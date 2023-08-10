97865197cbade1676e0f792c4f5d15e4d3194a59
eunjin pyeon <eunjin6324@gmail.com>
2023-08-10 23:17:21 +0900

    button 메소드 추가하기

--- LinqPj/Form1.Designer.cs
+++ LinqPj/Form1.Designer.cs
@@ -30,16 +30,16 @@
         {
             this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.titleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.artistDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.typeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.musicBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.titleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.artistDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.typeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.musicBindingSource = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.musicBindingSource)).BeginInit();
             this.SuspendLayout();
@@ -54,12 +54,62 @@
             this.typeDataGridViewTextBoxColumn,
             this.dateDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.musicBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(35, 42);
+            this.dataGridView1.Location = new System.Drawing.Point(31, 37);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.Size = new System.Drawing.Size(793, 414);
+            this.dataGridView1.Size = new System.Drawing.Size(777, 425);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(845, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(240, 56);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "전체보기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(845, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(240, 56);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "5월 이후에 나온 곡";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(845, 222);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(240, 56);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "제목 순서대로 전체보기";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(845, 313);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(240, 56);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "BIG Naughty곡만 보기";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(845, 406);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(240, 56);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "피쳐링 한 곡만 보기";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // titleDataGridViewTextBoxColumn
             // 
@@ -97,57 +147,11 @@
             // 
             this.musicBindingSource.DataSource = typeof(LinqPj.Music);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(855, 42);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(197, 64);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "전체보기";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(855, 126);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(197, 64);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "날짜 순서대로 전체 보기";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(855, 214);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(197, 64);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "제목 정렬 전체보기";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(855, 302);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(197, 64);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "피쳐링 한 곡만 보기";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(855, 392);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(197, 64);
-            this.button5.TabIndex = 6;
-            this.button5.Text = "BIG Naughty곡만 보기";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1081, 499);
+            this.ClientSize = new System.Drawing.Size(1132, 509);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
--- LinqPj/Form1.cs
+++ LinqPj/Form1.cs
@@ -14,26 +14,26 @@ namespace LinqPj
     {
         static private List<Music> musics = new List<Music>()
         {
-            new Music() { Title = "다시 걸을 때", Artist = "TOIL", Date = "0130", Type = "Feat"},
-            new Music() { Title = "정이라고 하자", Artist = "BIG Naughty", Date = "0420", Type = "Single"},
-            new Music() { Title = "Level", Artist = "J Word", Date = "0304", Type = "Feat"},
-            new Music() { Title = "Lovey Dovey", Artist = "BIG Naughty", Date = "0609", Type = "EP"},
-            new Music() { Title = "이건내가처음쓰는사랑노래", Artist = "키조", Date = "0705", Type = "Feat"},
-            new Music() { Title = "길", Artist = "기리보이", Date = "0225", Type = "Feat"},
-            new Music() { Title = "이브닝", Artist = "유주", Date = "0728", Type = "Feat"},
-            new Music() { Title = "낭만교향곡", Artist = "BIG Naughty", Date = "0609", Type = "EP"},
-            new Music() { Title = "Vancouver", Artist = "BIG Naughty", Date = "0609", Type = "EP"},
-            new Music() { Title = "Actor", Artist = "TOIL", Date = "0130", Type = "Feat"},
-            new Music() { Title = "단거", Artist = "고막소년단", Date = "1122", Type = "Pj"},
-            new Music() { Title = "연애의 발명", Artist = "BIG Naughty", Date = "0819", Type = "OST"},
-            new Music() { Title = "Drivin'", Artist = "김승민", Date = "0730", Type = "Feat"},
-            new Music() { Title = "Night Dancer", Artist = "imase", Date = "0515", Type = "Remix"},
-            new Music() { Title = "뻔한 발라드", Artist = "BIG Naughty", Date = "0228", Type = "EP"},
-            new Music() { Title = "친구로 지내다 보면", Artist = "BIG Naughty", Date = "0129", Type = "Single"},
-            new Music() { Title = "딱 10cm만", Artist = "BIG Naughty x 10cm", Date = "0925", Type = "Single"},
-            new Music() { Title = "여름 밤에 쓴 노래", Artist = "릴러말즈", Date = "0831", Type = "Prod"},
-            new Music() { Title = "부라보", Artist = "BIG Naughty", Date = "0225", Type = "EP"},
-            new Music() { Title = "Peace Out(MEGA MIX)", Artist = "블라세", Date = "0120", Type = "Feat"},
+            new Music() { Title = "다시 걸을 때", Artist = "TOIL", Date = 130, Type = "Feat"},
+            new Music() { Title = "정이라고 하자", Artist = "BIG Naughty", Date = 420, Type = "Single"},
+            new Music() { Title = "Level", Artist = "J Word", Date = 304, Type = "Feat"},
+            new Music() { Title = "Lovey Dovey", Artist = "BIG Naughty", Date = 609, Type = "EP"},
+            new Music() { Title = "이건내가처음쓰는사랑노래", Artist = "키조", Date = 705, Type = "Feat"},
+            new Music() { Title = "길", Artist = "기리보이", Date = 225, Type = "Feat"},
+            new Music() { Title = "이브닝", Artist = "유주", Date = 728, Type = "Feat"},
+            new Music() { Title = "낭만교향곡", Artist = "BIG Naughty", Date = 609, Type = "EP"},
+            new Music() { Title = "Vancouver", Artist = "BIG Naughty", Date = 609, Type = "EP"},
+            new Music() { Title = "Actor", Artist = "TOIL", Date = 130, Type = "Feat"},
+            new Music() { Title = "단거", Artist = "고막소년단", Date = 1122, Type = "Pj"},
+            new Music() { Title = "연애의 발명", Artist = "BIG Naughty", Date = 819, Type = "OST"},
+            new Music() { Title = "Drivin'", Artist = "김승민", Date = 730, Type = "Feat"},
+            new Music() { Title = "Night Dancer", Artist = "imase", Date = 515, Type = "Remix"},
+            new Music() { Title = "뻔한 발라드", Artist = "BIG Naughty", Date = 228, Type = "EP"},
+            new Music() { Title = "친구로 지내다 보면", Artist = "BIG Naughty", Date = 129, Type = "Single"},
+            new Music() { Title = "딱 10cm만", Artist = "BIG Naughty x 10cm", Date = 925, Type = "Single"},
+            new Music() { Title = "여름 밤에 쓴 노래", Artist = "릴러말즈", Date = 831, Type = "Prod"},
+            new Music() { Title = "부라보", Artist = "BIG Naughty", Date = 225, Type = "EP"},
+            new Music() { Title = "Peace Out(MEGA MIX)", Artist = "블라세", Date = 120, Type = "Feat"},
 
         };
 
@@ -52,7 +52,37 @@ namespace LinqPj
 
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            musicBindingSource.DataSource = musics;
+        }
 
-       
+        private void button2_Click(object sender, EventArgs e)
+        {
+            musicBindingSource.DataSource = from item in musics
+                                            where item.Date > 500
+                                            select item;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            musicBindingSource.DataSource = from item in musics
+                                            orderby item.Title
+                                            select item;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            musicBindingSource.DataSource = from item in musics
+                                            where item.Artist == "BIG Naughty"
+                                            select item;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            musicBindingSource.DataSource = from item in musics
+                                            where item.Type == "Feat"
+                                            select item;
+        }
     }
 }
